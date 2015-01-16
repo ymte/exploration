33909d3763f3cd156dd1e929a80c7b25707282ac
Jordi1080 <jordi.hesselz@hotmail.com>
2015-01-16 15:47:25 +0100

    basic window

--- src/exploration/Exploration.java
+++ src/exploration/Exploration.java
@@ -5,5 +5,13 @@ import org.lwjgl.glfw.*;
 public class Exploration {
 	public static void main(String[] args) {
 		GLFW.glfwGetTime();
+		long window = GLFW.glfwCreateWindow(800,600,"Testhoi",0,0);
+		GLFW.glfwMakeContextCurrent(window);
+		GLFW.glfwSwapInterval(1);
+		GLFW.glfwShowWindow(window);
+		
+		while (true){
+			
+		}
 	}
 }
