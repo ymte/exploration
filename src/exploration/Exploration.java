5123ab4942430c70bb6bdb5a0c90c52b2eab6863
ymte <ymto23@hotmail.com>
2015-01-16 15:54:26 +0100

    basic window for real

--- src/exploration/Exploration.java
+++ src/exploration/Exploration.java
@@ -1,16 +1,27 @@
 package exploration;
 
+import org.lwjgl.Sys;
 import org.lwjgl.glfw.*;
+import org.lwjgl.opengl.*;
+
+import static org.lwjgl.glfw.Callbacks.*;
+import static org.lwjgl.glfw.GLFW.*;
+import static org.lwjgl.opengl.GL11.*;
+import static org.lwjgl.system.MemoryUtil.*;
 
 public class Exploration {
 	public static void main(String[] args) {
-		GLFW.glfwGetTime();
-		long window = GLFW.glfwCreateWindow(800,600,"Testhoi",0,0);
-		GLFW.glfwMakeContextCurrent(window);
-		GLFW.glfwSwapInterval(1);
-		GLFW.glfwShowWindow(window);
+		glfwInit();
 		
-		while (true){
+		glfwSetErrorCallback(errorCallbackPrint(System.err));
+		
+		glfwDefaultWindowHints();
+		long window = glfwCreateWindow(1280,720,"Testhoi",0,0);
+		glfwMakeContextCurrent(window);
+		glfwSwapInterval(1);
+		glfwShowWindow(window);
+		
+		while (true) {
 			
 		}
 	}
